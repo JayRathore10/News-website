c8dd0b5dd179d5fd081b9eea111fd6c15cbe9dfa
Jay <jayrathore88155@gmail.com>
2025-10-26 14:47:26 +0530

    Fetch Data from the Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { useEffect , useState } from 'react';
+import axios from 'axios';
 import './App.css';
+
 function App(){
+
+  const [articles , setArticles] = useState([]);
+
+  const apiKey = `a6060425be3f4ac19c6fb8ee9a680772`;
+
+  useEffect(()=>{
+    const fetchNews = async()=>{
+      try{
+        const res = await axios.get(`https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=${apiKey}`);
+        setArticles(res.data.articles);
+        console.log(res.data.articles);
+      }catch(error){
+        console.log(error);
+      }     
+    }
+    fetchNews();
+  } , [apiKey]);  
+
   return(
     <>
-    
+      {
+        articles.map((news, index)=>(
+           <p
+            key={index}
+          >
+            {news.title}
+          </p>
+        ))
+      }
     </>
   );
 }
