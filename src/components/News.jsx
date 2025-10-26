46f207bbf245ca661b52e6bcb30310ff0e844d6b
Jay <jayrathore88155@gmail.com>
2025-10-26 17:24:50 +0530

    Add favicon

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,15 +1,15 @@
 import './News.css';
 
-export function News({news}){
-  return(
-    <>
-      <div className='news-div'>
-        <h1>{news.title}</h1>
-        <a href={news.url} target='_blank'>
-          <img src={news.urlToImage} alt={news.title} className='news-img'/>
-        </a>
+export function News({ news }) {
+  return (
+    <div className="news-div">
+      <a href={news.url} target="_blank" rel="noopener noreferrer">
+        <img src={news.urlToImage} alt={news.title} className="news-img" />
+      </a>
+      <div className="news-des">
+        <h1 className="news-heading">{news.title}</h1>
         <p>{news.description}</p>
       </div>
-    </>
+    </div>
   );
 }
